91aa234800d43274f85cda8172aae88cd1cfe304
Oleksandr Skrypnyk <oskryp@Oleksandrs-iMac.local>
2014-10-09 00:18:50 +0300

    Initial Commit

---
+++ POSIX_threads/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  POSIX_threads
+//
+//  Created by Oleksandr Skrypnyk on 10/9/14.
+//  Copyright (c) 2014 Oleksandr Skrypnyk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
